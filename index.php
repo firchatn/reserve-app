283645481cfc98166f6cf436c9e7739ffa5ffeff
firchtn <firaschaabencss@gmail.com>
2017-06-27 16:35:40 +0100

    change dynamique table

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php
+echo"
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -42,40 +44,26 @@ color : green;
 </head>
 
 <body>
-<?php
+<h1> tableaux de reservation </h1>";
+
 if($id=mysql_connect("localhost","root","")){
 		if($id_db=mysql_select_db("lavage")){
-			echo "succés de connexion<br/>";
 echo "<table  id='customers'>
 <tr>
 <td></td><td>8 - 9 </td><td>9 - 10 </td><td>10 - 11 </td><td>11 - 12 </td><td>12 - 13 </td><td>14 - 15 </td> <td>15 - 16 </td> <td>16 - 17 </td> <td>17 - 18 </td>
-</tr>
-<tr>
-<td>lundi </td><td><a href='inscri.html' id='l'> libre </a></td><td>libre </td><td>libre </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td>
-</tr>
-<tr>
-<td>mardi </td><td>libre </td><td>libre </td><td>libre </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td>
-</tr>
-<tr>
-<td>mercredi </td><td>libre </td><td>libre </td><td>libre </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td>
-</tr>
-<tr>
-<td>jeudi </td><td>libre </td><td>libre </td><td>libre </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td>
-</tr>
-<tr>
-<td>vendredi </td><td>libre </td><td>libre </td><td>libre </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td>
-</tr>
-<tr>
-<td>samedi </td><td>libre </td><td>libre </td><td>libre </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td>
-</tr>
-<tr>
-<td>dimanche </td><td>libre </td><td>libre </td><td>libre </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td>
-</tr>
-</table>";
-?>
-</body>
-</html>
+</tr>";
+if($resultat=mysql_query("select * from clients;")){
+				while($ligne=mysql_fetch_row($resultat)){
 
+echo"<tr>
+<td>$ligne[0] </td><td><a href='inscri.html' id='l'> $ligne[1] </a></td><td>libre </td><td>libre </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td><td>oucuper </td>
+</tr>
+
+</table>
+
+</body>
+</html>";
+?>
 
 
 
